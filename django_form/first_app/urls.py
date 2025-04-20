c1ce2fd19dbf79048c1a4a106bf3877959529314
tanvir-ahammedd <tanvircolorful@gmail.com>
2025-04-20 10:58:38 +0600

    Validating Form Data; attributes and widget

--- django_form/first_app/urls.py
+++ django_form/first_app/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='homepage'),
     path('about/', views.about, name='aboutpage'),
-    path('django_form/', views.DjangoForm, name='django_form')
+    path('django_form/', views.StudentForm, name='django_form')
 ]
--- django_form/first_app/views.py
+++ django_form/first_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . forms import contactForm
+from . forms import StudentData
 # Create your views here.
 def home(request):
     return render(request, 'first_app/home.html')
@@ -12,3 +13,13 @@ def DjangoForm(request):
     if form.is_valid():
         print(form.cleaned_data)
     return render(request, 'first_app/django_form.html', {'form': form})
+
+def StudentForm(request):
+    if request.method == 'POST':
+        form = StudentData(request.POST, request.FILES)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = StudentData()
+    return render(request, 'first_app/django_form.html', {'form':form})
+       
