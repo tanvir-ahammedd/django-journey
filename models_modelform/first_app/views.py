f8af6b9f1afa3174ebca0b93481878ceb00ac8ae
tanvir-ahammedd <tanvircolorful@gmail.com>
2025-04-21 16:28:29 +0600

    Showing Model data to fronted

---
+++ models_modelform/first_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from . import models
+# Create your views here.
+def home(request):
+    student = models.Student.objects.all()
+    print(student)
+    return render(request, 'home.html', {'data': student})
